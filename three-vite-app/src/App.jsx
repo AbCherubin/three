60594b46adadc0386c92fd17fe963f36983f32ef
abcherubin <ppst.801@gmail.com>
2023-09-06 21:45:18 +0700

    lighting

--- three-vite-app/src/App.jsx
+++ three-vite-app/src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ThreeScene from './components/ThreeScene';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ThreeScene from "./components/ThreeScene";
 function App() {
-  const [count, setCount] = useState(0)
-  console.log("APP")
+  const [count, setCount] = useState(0);
   return (
     <>
       <div className="app">
-      <ThreeScene />
+        <ThreeScene />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
